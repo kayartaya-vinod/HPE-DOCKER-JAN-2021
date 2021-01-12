90c9ee8a8e7fd3a86238bc937cc50315c6da38ea
Vinod Kumar Kayartaya <kayartaya.vinod@gmail.com>
2021-01-12 14:30:14 +0530

    add dynamic hostname/portnum

--- springboot/src/main/java/com/hpe/DockerDemoApplication.java
+++ springboot/src/main/java/com/hpe/DockerDemoApplication.java
@@ -3,10 +3,12 @@ package com.hpe;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @SpringBootApplication
@@ -15,7 +17,20 @@ public class DockerDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DockerDemoApplication.class, args);
 	}
-	
+
+	//	docker run -e FLASK_HOST_NAME=python-flask-app -e FLASK_PORT_NUMER=5000 --rm -d ...
+	@Value("${FLASK_HOST_NAME:flaskapp}")
+	String flaskHostName;
+
+	@Value("${FLASK_PORT_NUMBER:1234}")
+	String flaskPortNumber;
+
+	@GetMapping("/api/flaskapp-author")
+	public String flaskappAuthorInfo() {
+		String url = "http://" + flaskHostName + ":" + flaskPortNumber + "/api/author";
+		RestTemplate template = new RestTemplate();
+		return template.getForEntity(url, String.class).getBody();
+	}
 
 	@GetMapping("/api/author")
 	public Map<String, String> authorInfo() {
